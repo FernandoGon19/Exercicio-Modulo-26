d0434755f41a439c1a86afaee208d414b7cd84b9
Fernando Gonçalves Prudente <129014797+FernandoGon19@users.noreply.github.com>
2023-11-27 14:14:38 -0300

    Add files via upload

---
+++ Exercicio26 - TypeScript/src/Exercicio26.js
@@ -0,0 +1,9 @@
+"use strict";
+function calculaNumeros(num1, num2) {
+    return num1 * num2;
+}
+console.log(calculaNumeros(10, 10));
+function retornaOi(nome) {
+    return 'Olá ' + nome;
+}
+console.log(retornaOi('Fernando'));
---
+++ Exercicio26 - TypeScript/src/Exercicio26.ts
@@ -0,0 +1,11 @@
+function calculaNumeros( num1: number, num2: number): number {
+     return num1 * num2
+}
+
+console.log(calculaNumeros(10, 10))
+
+function retornaOi (nome: string): string {
+     return 'Olá '+ nome
+}
+
+console.log(retornaOi('Fernando'))
